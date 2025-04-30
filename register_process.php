c972624051be75be4b37300d990466d5218502a2
Adi Lakshmi Medapati <adilakshmi_medapati@srmap.edu.in>
2025-04-30 10:08:49 +0530

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($stmt->execute()) {
         // Redirect to login page after successful registration
-        header("Location: index.php");
+        header("Location: index.html");
         exit();
     } else {
         echo "Error: " . $stmt->error;
